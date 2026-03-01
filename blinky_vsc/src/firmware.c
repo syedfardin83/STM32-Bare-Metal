60fcfb666dd76e5f2ccdeeebbfcd201ff2d3cd4a
Syed Fardin <syedfardin0612@gmail.com>
2026-03-01 11:21:53 +0530

    blinky code done

--- blinky_vsc/src/firmware.c
+++ blinky_vsc/src/firmware.c
@@ -1,13 +1,32 @@
+#include <stdint.h>
 #include <libopencm3/include/libopencm3/stm32/rcc.h>
+#include <libopencm3/include/libopencm3/stm32/gpio.h>
+
+#define LED_PORT (GPIOB)
+#define LED_PIN (GPIO5)
 
 static void rcc_setup(void){
-
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_CONFIG_HSI_32MHZ]);
 }
 
-int main(){
-    while(1){
+static void gpio_setup(void){
+    rcc_periph_clock_enable(GPIOB);
+    gpio_mode_setup(LED_PORT,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,LED_PIN);
+}
 
+static void delay_cycles(uint32_t cycles){
+    for(uint32_t i=0;i<cycles;i++){
+        __asm__("nop");
+    }
+}
+
+int main(){
+    rcc_setup();
+    gpio_setup();
+
+    while(1){
+        gpio_toggle(LED_PORT,LED_PIN);
+        delay_cycles(32000000/4);
     }
     return 0;
 }
