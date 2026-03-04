f698093ca54a068fc544cc9adb8f0171859548db
Syed Fardin <syedfardin0612@gmail.com>
2026-03-04 16:51:21 +0530

    SysTick timer code; not tested.

--- blinky_vsc/src/firmware.c
+++ blinky_vsc/src/firmware.c
@@ -1,9 +1,24 @@
 #include <stdint.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/systick.h>
+#include <libopencm3/cm3/vector.h>
 
-#define LED_PORT (GPIOB)
-#define LED_PIN (GPIO5)
+#define LED_PORT      (GPIOB)
+#define LED_PIN       (GPIO5)
+
+#define CLOCK_FREQ    (32000000)
+#define SYSTICK_FREQ  (1000)
+
+volatile uint64_t ticks=0;
+void sys_tick_handler(void){
+    ticks++;
+}
+
+static uint64_t get_ticks(void){
+    return ticks;
+}
+
 
 static void rcc_setup(void){
     rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_CONFIG_HSI_32MHZ]);
@@ -14,19 +29,24 @@ static void gpio_setup(void){
     gpio_mode_setup(LED_PORT,GPIO_MODE_OUTPUT,GPIO_PUPD_NONE,LED_PIN);
 }
 
-static void delay_cycles(uint32_t cycles){
-    for(uint32_t i=0;i<cycles;i++){
-        __asm__("nop");
-    }
+static void systick_setup(void){
+    systick_set_frequency(SYSTICK_FREQ, CLOCK_FREQ);
+    systick_counter_enable();
+    systick_interrupt_enable();
 }
+
 
 int main(){
     rcc_setup();
     gpio_setup();
 
+    uint64_t start_time = get_ticks();
+
     while(1){
-        gpio_toggle(LED_PORT,LED_PIN);
-        delay_cycles(32000000/16);
+        if(get_ticks()-start_time >= 1000){
+            gpio_toggle(LED_PORT,LED_PIN);
+            start_time = get_ticks();
+        }
     }
     return 0;
 }
