691c9fbdd347af4a679386760437b69dcc6c51d3
Syed Fardin <syedfardin0612@gmail.com>
2026-03-14 21:20:36 +0530

    timer setup started for PWM: GPIO PB3 --> TIM2_CH2

--- blinky_vsc/src/core/timer.c
+++ blinky_vsc/src/core/timer.c
@@ -6,7 +6,16 @@
 // GPIO PB3 --> TIM2_CH2
 void timer_setup(void){
     rcc_periph_clock_enable(RCC_TIM2);
+
+    //  High level timer setup
     timer_set_mode(TIM2,TIM_CR1_CKD_CK_INT,TIM_CR1_CMS_EDGE,TIM_CR1_DIR_UP);
+
+    //  Set the mode to PWM on channel 2.
+    timer_set_oc_mode(TIM2,TIM_OC2,TIM_OCM_PWM1);
+
+    //  Enable counter
+    timer_enable_counter(TIM2);
+    timer_enable_oc_output(TIM2,TIM_OC2);
 }
 
 void timer_pwm_set_duty_cycle(float duty_cycle){
