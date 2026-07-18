02c0d5020752e786ec8c1d947385f6bca3de82a7
Syed Fardin <syedfardin0612@gmail.com>
2026-07-18 14:40:42 +0530

    bootloader project started

---
+++ bootloader/src/firmware.c
@@ -0,0 +1,15 @@
+#include "common-defines.h"
+
+#define BOOTLOADER_SIZE     (0x8000)
+#define MAIN_APP_STARTS     (0x08008000)
+
+
+void jump_to_main(){
+
+}
+
+
+int main(){
+    jump_to_main();
+    return 0;
+}
