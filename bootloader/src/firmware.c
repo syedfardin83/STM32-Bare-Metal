8ab3023a0e9b0193d1ad8e4056f1f9ad76ac4728
Syed Fardin <syedfardin0612@gmail.com>
2026-07-22 11:51:44 +0530

    jump_to_main function

--- bootloader/src/firmware.c
+++ bootloader/src/firmware.c
@@ -1,11 +1,19 @@
 #include "common-defines.h"
+#include "libopencm3/stm32/memorymap.h"
 
-#define BOOTLOADER_SIZE     (0x8000)
-#define MAIN_APP_STARTS     (0x08008000)
+#define BOOTLOADER_SIZE            (0x8000)
+#define MAIN_APP_START_ADDRESS     (FLASH_BASE + BOOTLOADER_SIZE)
+
+
 
 
 void jump_to_main(){
+    uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS+4U);
+    void* reset_vector = (void*)(*reset_vector_entry);
+    void (*jump_func)(void);
+    jump_func = (void (*)(void))reset_vector;
 
+    jump_func();
 }
 
 
