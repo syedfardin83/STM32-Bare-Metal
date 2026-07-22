4091546217ae1c64585faf9ae5af81637a528601
Syed Fardin <syedfardin0612@gmail.com>
2026-07-22 12:36:05 +0530

    jump_to_main

---
+++ bootloader/src/firmware.d
@@ -0,0 +1,12 @@
+firmware.o: firmware.c ../inc/common-defines.h \
+ /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdbool.h \
+ ../libopencm3/include/libopencm3/stm32/memorymap.h \
+ ../libopencm3/include/libopencm3/stm32/l0/memorymap.h \
+ ../libopencm3/include/libopencm3/cm3/memorymap.h
